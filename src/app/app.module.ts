4b4004c5622d72541ba2a02f7c907b9ceab62d62
abdelrouf-ali <abdelrouf65@gmail.com>
2023-06-29 07:01:18 +0300

    clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    RegisterComponent,
-    NavComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
