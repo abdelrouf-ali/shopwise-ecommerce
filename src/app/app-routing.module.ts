6efe9d4b9add5205fa0047c3f39d894d0eaad2c6
amrhossamo <amorahossam1@gmail.com>
2023-07-08 04:11:41 +0300

    display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 const routes: Routes = [
-  { path: 'products', component: ProductsComponent },
   { path: 'products/:id', component: ProductDetailsComponent }
 ];
 
