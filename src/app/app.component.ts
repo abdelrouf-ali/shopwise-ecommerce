c643e71a481a90361ed2cfcaed2c31a213fdefb5
amrhossamo <amorahossam1@gmail.com>
2023-07-12 06:13:43 +0300

    login-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faCoffee} from '@fortawesome/free-solid-svg-icons'
 
 
 @Component({
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  icon=faCoffee;
 }
